f27c6d083621e25cd66c8e67283ede8bebb2191a
Juan Camilo Salazar <jc.salazarquintero@gmail.com>
2025-09-14 15:32:03 -0500

    first commit

---
+++ src/main/java/co/analisys/biblioteca/service/NotificacionService.java
@@ -0,0 +1,16 @@
+package co.analisys.biblioteca.service; 
+ 
+import co.analisys.biblioteca.dto.NotificacionDTO; 
+import org.springframework.kafka.annotation.KafkaListener; 
+import org.springframework.stereotype.Service; 
+ 
+@Service 
+public class NotificacionService { 
+ 
+
+    public void enviarNotificacion(NotificacionDTO notificacion) { 
+        // Simular envío de notificación 
+        System.out.println("Notificación enviada a " + notificacion.getUsuarioId() + ": " + 
+notificacion.getMensaje()); 
+    } 
+} 
